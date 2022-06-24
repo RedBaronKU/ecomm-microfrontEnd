1e9a61274022e3942e583961d9a9f1c5bc50c704
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-06-24 10:59:39 +0530

    cart changes: 1) standalone vs export
    2) both products and cart have different ids for standalone display
    
    3) Now different name in container so they can gel up

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,16 @@
 import faker from "faker";
 
-const cartText = `<div>You Have ${faker.datatype.number()} items</div>`;
+const mount = (el) => {
+  const cartText = `<div>You Have ${faker.datatype.number()} items</div>`;
+  el.innerHTML = cartText;
+};
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#cart-dev");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: "cart",
       filename: "remoteEntry",
       exposes: {
-        "./CartShow": "./src/index",
+        "./CartShow": "./src/bootstrap",
       },
     }),
     new HtmlWebpackPlugin({
